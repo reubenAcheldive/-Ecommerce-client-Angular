585ad1272e0a3752cef873f1c539d3c53f97ab27
Your Name <you@example.com>
2022-06-23 11:53:14 +0300

    deleted all componnents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes, PreloadAllModules, CanActivate} from '@angular/router';
 import {AuthGuard} from './services/AuthGuard/AuthGuard.guard';
 import {AuthGuardAdmin} from './services/AuthGuard/AuthGurdAdmin';
-const routes: Routes = [
-    {
-        path: 'register',
-        loadChildren: () => import ('./component/userAuth/registration/registration.module').then((m) => m.RegistrationModule)
-    }, {
-        path: 'online',
-        loadChildren: () => import ('./component/dashboard/dashboard.module').then((m) => m.DashboardModule)
-    }, {
-        path: '**',
-        redirectTo: '/online',
-        pathMatch: 'full'
-    },
-];
+// const routes: Routes = [
+
+// ];
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
+        RouterModule.forRoot(null, {preloadingStrategy: PreloadAllModules}),
     ],
     exports: [RouterModule]
 })export class AppRoutingModule {}
