039aba15f1ea100e6ea96bcebbba178a024df1b4
Your Name <you@example.com>
2022-06-23 12:03:43 +0300

    build dashbaord mudole

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes, PreloadAllModules, CanActivate} from '@angular/router';
 import {AuthGuard} from './services/AuthGuard/AuthGuard.guard';
 import {AuthGuardAdmin} from './services/AuthGuard/AuthGurdAdmin';
-// const routes: Routes = [
+const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) }
 
-// ];
+];
 @NgModule({
     imports: [
-        RouterModule.forRoot(null, {preloadingStrategy: PreloadAllModules}),
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
     ],
     exports: [RouterModule]
 })export class AppRoutingModule {}
+//"catagories"
