fef19650feaa5ef66d50856c62104d1dafc63fd4
Your Name <you@example.com>
2022-06-24 18:08:01 +0300

    create folde structer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes, PreloadAllModules, CanActivate} from '@angular/router';
 import {AuthGuard} from './services/AuthGuard/AuthGuard.guard';
 import {AuthGuardAdmin} from './services/AuthGuard/AuthGurdAdmin';
-const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) }
-
-];
+const routes: Routes = [{
+        path: 'dashboard',
+        loadChildren: () => import ('./components/dashboard/dashboard.module').then(m => m.DashboardModule)
+    }];
 @NgModule({
     imports: [
         RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
     ],
     exports: [RouterModule]
 })export class AppRoutingModule {}
-//"catagories"
+// "catagories"
