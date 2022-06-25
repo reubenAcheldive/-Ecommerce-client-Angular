e4baab5f9115d2e1514ef9ed077c1ae5df0a91ad
Your Name <you@example.com>
2022-06-25 16:52:29 +0300

    creat cart module

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [DashboardComponent, NavBarComponent],
-  imports: [CommonModule, DashboardRoutingModule, MatToolbarModule],
+  imports: [CommonModule, DashboardRoutingModule, MatToolbarModule,MatSidenavModule],
 })
 export class DashboardModule {}
